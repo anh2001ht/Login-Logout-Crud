7eeb8e2ba1682e3eb7ac3711a28debae65281338
HoangAnh <anh2001httttt@gmail.com>
2022-11-25 14:01:58 +0700

    done

--- MyCode_EntityFramework/Controllers/HomeController.cs
+++ MyCode_EntityFramework/Controllers/HomeController.cs
@@ -25,46 +25,7 @@ namespace MyCode_EntityFramework.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public ActionResult Login(string username, string password)
-        /*{
-            if (username == "ha1" && password == "123")
-            {
-                HttpContext.Session.SetString("username", "username");
-                //return View("Success");
-                return RedirectToAction("Index", "Usertbls");
-            }
-            else
-            {
-                ViewBag.error = "Invalid Account";
-                return View("Index");
-            }
-        }*/
-
-        {
-            using (DB_Entities db = new DB_Entities())
-            {
-                var userDetail = db.user.Where(x => x.UserName == username && x.Password == password).FirstOrDefault();
-                string name = username;
-                HttpContext.Session.SetString("UserID", name);
-                if (userDetail != null)
-                {
-                    return RedirectToAction("Index", "Usertbls");
-                }
-                else
-                {
-                    ViewBag.error = "Invalid Account";
-                    return View("Index");
-                }
-
-            }
-        }
-        [HttpGet]
-        public ActionResult Logout()
-        {
-            HttpContext.Session.Remove("username");
-            return RedirectToAction("Index");
-        }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
